c8f1009008dccbae86388783a7f2af100ade8067
Johnathan Meunier <johnathan@MacBook-Pro-de-Johnathan.local>
2013-12-12 17:41:27 +0100

    fix index, listview of name

---
+++ src/com/supinfo/supcrowdfunder/entity/DateAdapter.java
@@ -0,0 +1,23 @@
+package com.supinfo.supcrowdfunder.entity;
+/*
+import javax.xml.bind.annotation.adapters.*;
+
+public class DateAdapter extends XmlAdapter<java.util.Date, java.sql.Date> {
+
+    @Override
+    public java.util.Date marshal(java.sql.Date sqlDate) throws Exception {
+        if(null == sqlDate) {
+            return null;
+        }
+        return new java.util.Date(sqlDate.getTime());
+    }
+
+    @Override
+    public java.sql.Date unmarshal(java.util.Date utilDate) throws Exception {
+        if(null == utilDate) {
+            return null;
+        }
+        return new java.sql.Date(utilDate.getTime());
+    }
+
+}*/
