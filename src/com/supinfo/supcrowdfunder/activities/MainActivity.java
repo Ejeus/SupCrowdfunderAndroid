62f23ef9438c1cf2a1f51e69ce59646654eab900
Johnathan Meunier <johnathan@MacBook-Pro-de-Johnathan.local>
2013-12-13 20:30:51 +0100

    fix index bug

--- src/com/supinfo/supcrowdfunder/activities/MainActivity.java
+++ src/com/supinfo/supcrowdfunder/activities/MainActivity.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import java.lang.reflect.Array;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
@@ -19,19 +18,16 @@ import org.json.JSONObject;
 
 
 import com.supinfo.supcrowdfunder.R;
-import com.supinfo.supcrowdfunder.adapter.ProjectAdapter;
 import com.supinfo.supcrowdfunder.entity.Project;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -39,39 +35,34 @@ import android.widget.AdapterView.OnItemClickListener;
 public class MainActivity extends Activity {
 	
 	private ListView listViewProjects;
-	private ProjectAdapter adapterProject;
-	ArrayList<Project> projects = new ArrayList<Project>();
-	private LinearLayout linearLayoutProjects;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		Project project = new Project();
+		
 		listViewProjects = (ListView) this.findViewById(R.id.listViewProjects);
 		
 	    JSONObject obj = sendGetRequest("http://192.168.0.13:8080/SupCrowdFunder/resources/index");
 	    
+	    
+	    ArrayList<String> list = new ArrayList<String>();
 	    JSONArray array;
 		try {
 			array = obj.getJSONArray("project");
 		    for(int i = 0 ; i < array.length() ; i++){
-		    	//projects.add(array.getJSONObject(i).getString("name"));
-		    	addProject(array.getJSONObject(i).getString("name"));
+		        list.add(array.getJSONObject(i).getString("name"));
 		    }
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		/*
+		
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_list_item_1, list);
 		listViewProjects.setAdapter(adapter);
-		
-		adapterProject = new ProjectAdapter(this, projects);
-		listViewProjects.setAdapter(adapterProject);*/
-		
-		listViewProjects.setVisibility(View.GONE);
 		
 		listViewProjects.setOnItemClickListener(new OnItemClickListener() {
 			@Override
@@ -106,19 +97,7 @@ public class MainActivity extends Activity {
 	  return myObject;
 	}
 	
-	public void addProject(String strProject) {
-		Project project = new Project();
-		project.setName(strProject);
-		projects.add(project);
-		
-		TextView textView = new TextView(this);
-		textView.setText(strProject);
-		if(projects.size()%2 == 1)
-			textView.setBackgroundColor(Color.BLUE);
-		else
-			textView.setBackgroundColor(Color.GREEN);
-		linearLayoutProjects.addView(textView);
-	}
+	
 
 
 	
