e2fb9b41f6f9c81a99b57c21c8d689e5cb4a85d1
Johnathan Meunier <johnathan@MacBook-Pro-de-Johnathan.local>
2013-12-13 17:29:20 +0100

    Add project Adatpter

---
+++ src/com/supinfo/supcrowdfunder/adapter/ProjectAdapter.java
@@ -0,0 +1,57 @@
+package com.supinfo.supcrowdfunder.adapter;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.supinfo.supcrowdfunder.entity.Project;
+
+public class ProjectAdapter extends BaseAdapter {
+	
+	private Context context;
+	private ArrayList<Project> projects;
+	
+	public ProjectAdapter(Context context, ArrayList<Project> projects) {
+		this.context = context;
+		this.projects = projects;
+	}
+	
+	public void setQuotes(ArrayList<Project> projects) {
+		this.projects = projects;
+	}
+
+	@Override
+	public int getCount() {
+		return projects.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return projects.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		TextView textView = new TextView(context);
+		textView.setTextColor(Color.WHITE);
+		textView.setTextSize(18);
+		textView.setPadding(0, 0, 0, 30);
+		textView.setText(projects.get(position).getName());
+		if(position%2 == 0)
+			textView.setBackgroundColor(Color.DKGRAY);
+		else
+			textView.setBackgroundColor(Color.BLACK);
+		return textView;
+	}
+
+}
