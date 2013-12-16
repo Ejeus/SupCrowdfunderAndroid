e89e3db30dde9331d06f6e48f0af5850640a632e
DCKT <137479@supinfo.com>
2013-12-17 00:36:02 +0100

    Create editProfilActivity + View

--- src/com/supinfo/supcrowdfunder/activities/ProfileActivity.java
+++ src/com/supinfo/supcrowdfunder/activities/ProfileActivity.java
@@ -4,7 +4,10 @@ import com.supinfo.supcrowdfunder.R;
 import com.supinfo.supcrowdfunder.entity.User;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class ProfileActivity extends Activity {
@@ -20,5 +23,28 @@ public class ProfileActivity extends Activity {
 		TextView fullNameView = (TextView) findViewById(R.id.full_name);
 		fullNameView.setText(user.getFullName());
 	}
+	
+	public boolean onCreateOptionsMenu(Menu menu) {
+	
+		
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.profile, menu);
+		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	            
+	        case R.id.edit:
+	        	Intent profileIntent = new Intent(this, EditProfileActivity.class);
+	        	startActivityForResult(profileIntent, 0);
+	            return true;
+	            
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+
 
 }
